29e91610c95fab90d17d4f369f926ada849a1259
Hamidulloh07 <107644303+Hamidulloh07@users.noreply.github.com>
2022-10-04 01:05:23 +1400

    addd

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const TodoItem = ({index, todo, removeTodoHandler, checkTodoHandler}) => {
   return (
-    <li key={index + 1} className='d-flex align-items-center  list-group-item mt-4 w-25'>
+    <li key={index + 1} className='d-flex align-items-center  list-group-item mt-4 w-50'>
     <span className='text-danger me-3 d-inline-block'>{++index}</span>
     <input className="me-3" type={'checkbox'} onChange={() => checkTodoHandler(todo.id)} checked={todo.isCompleted}></input>
     <p className='text-warning m-0 fs-3 me-3' style={{textDecoration: todo.isCompleted ?  "line-through": "none"}}>{todo.title}</p>
